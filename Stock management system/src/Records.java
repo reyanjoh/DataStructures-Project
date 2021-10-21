af43c15b3769d51c19b7616a8a396cef6fee9179
Reyan John Narvaja <73632471+reyanjoh@users.noreply.github.com>
2021-10-21 13:50:10 +0800

    added: timestamp, display format

--- Stock management system/src/Records.java
+++ Stock management system/src/Records.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Records {
@@ -7,6 +9,10 @@ public class Records {
 
     public void enterStock() throws IOException {
         
+        Date date = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+        SimpleDateFormat time = new SimpleDateFormat("hh:mm");
+
         
 
         Scanner scan = new Scanner(System.in);
@@ -18,9 +24,19 @@ public class Records {
             PrintWriter pw = new PrintWriter(fw);
 
             System.out.println("Enter new stock: ");
-            System.out.println("Quantity, Product name");
-            String stock = scan.nextLine();
-            pw.println(stock);
+            System.out.print("Quantity: ");
+            String quantity = scan.nextLine();
+            System.out.print("Product: ");
+            String name = scan.nextLine();
+                if(Integer.parseInt(quantity)<10){
+                    pw.println( " [ " +dateFormat.format(date)+ " ] [ " + time.format(date) + " ] [  000" + quantity + "  ] [ " + name + " ] ");
+                }else if(Integer.parseInt(quantity)<100){
+                    pw.println( " [ " +dateFormat.format(date)+ " ] [ " + time.format(date) + " ] [  00" + quantity + "  ] [ " + name + " ] ");
+                }else if(Integer.parseInt(quantity)<1000){  
+                    pw.println( " [ " +dateFormat.format(date)+ " ] [ " + time.format(date) + " ] [  0" + quantity + "  ] [ " + name + " ] ");
+                }else if(Integer.parseInt(quantity)<10000){
+                    pw.println( " [ " +dateFormat.format(date)+ " ] [ " + time.format(date) + " ] [  " + quantity + "  ] [ " + name + " ] ");
+                }
             pw.close();
 
             System.out.print("\033[H\033[2J"); 
@@ -37,6 +53,7 @@ public class Records {
         scan.close();
     }
     
+
     public void viewStock() throws IOException {
 
         System.out.print("\033[H\033[2J"); 
@@ -45,9 +62,9 @@ public class Records {
             Scanner scan = new Scanner(file);
 
             if (scan.hasNextLine()) {
-                System.out.println("Quantity, Product name");
+                System.out.println("|     Date     |   Time  | Quantity | Product name|");
                 while (scan.hasNextLine()) {
-                    System.out.println( scan.nextLine());
+                    System.out.println(scan.nextLine());
                 }
             }else{
                 System.out.println("No records. . .");
